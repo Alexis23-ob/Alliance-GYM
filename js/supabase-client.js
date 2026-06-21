54aa173bbbe3bbbea009c0829713c060e22be76a
Alexis23-ob <Alexis23-ob@users.noreply.github.com>
2026-06-21 15:51:43 -0600

    fix: Corregir error de inicializacion de supabase client

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -3,6 +3,7 @@ const supabaseUrl = 'https://fxmfkftqpakgljovlacf.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ4bWZrZnRxcGFrZ2xqb3ZsYWNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwNjE5ODcsImV4cCI6MjA5NzYzNzk4N30.fZhMlj0liualhEnEA8cvFCyHUpuPB3yP854yCydFNE4';
 
 // Crear el cliente de Supabase
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+const supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
+window.supabase = supabaseClient; // Reasignar globalmente para que el resto del código funcione
 
 console.log("Supabase Client Initialized");
